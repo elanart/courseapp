a4f8d4287739aba062cd17c486e20d842747106e
elanart <elanart.06@gmail.com>
2024-03-25 22:52:29 +0700

    [Lan] - Thuc hanh den trang 25_django

---
+++ courseapp/courses/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+class ModelBase(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='courses/%Y/%m', null=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-id']
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Course(ModelBase):
+    subject = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('subject', 'category')
+
+    def __str__(self):
+        return self.subject
+    
+
+class Lesson(ModelBase):
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', related_query_name='my_lession')
+
+    class Meta:
+        unique_together = ('subject', 'course')
+
+    def __str__(self):
+        return self.subject
+    
